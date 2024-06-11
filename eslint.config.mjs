e690ae1c03327baab2350d9c8cc1eda0043315e5
Davis Chan <davis.chan@aol.com>
2024-06-12 00:38:10 +0800

    use ts parser for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,19 @@
 import pluginVue from 'eslint-plugin-vue'
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier'
 export default [
   // add more generic rulesets here, such as:
   // js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
   eslintConfigPrettier,
+  ...pluginVue.configs['flat/recommended'],
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  {
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+      }
+    }
+  },
   {
     rules: {
       // override/add rules settings here, such as:
