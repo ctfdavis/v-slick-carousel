9046e6d09d6d8d3f27b17781abc20131b7c4dd14
Davis <davisc@midland.com.hk>
2024-08-29 17:26:57 +0800

    wip: adding stories

--- packages/storybook/src/VSlickCarousel.stories.ts
+++ packages/storybook/src/VSlickCarousel.stories.ts
@@ -1,5 +1,6 @@
 import { VSlickCarousel } from 'v-slick-carousel'
 import 'v-slick-carousel/style.css'
+import './style.css'
 import { h } from 'vue'
 
 export default {
@@ -7,40 +8,65 @@ export default {
   title: 'VSlickCarousel',
   tags: ['autodocs'],
   args: {
-    default: defaultSlides
+    default: defaultSlides,
+    dots: true,
+    infinite: false,
+    groupsToShow: 1,
   },
   argTypes: {
     default: {
-      control: {
-        type: 'text'
-      },
-      description: 'The default slot content.',
+      description: 'Slot for slides.',
       table: {
         category: 'slots',
         type: {
           summary: 'any'
         }
       }
+    },
+    dots: {
+      description: 'Show the dots navigation',
+    },
+    infinite: {
+      description: 'Infinite loop sliding',
+    },
+    groupsToShow: {
+      description: 'Number of slide groups to show',
     }
   }
 }
 
-export const Default = {
+export const OneSlideFinite = {
   args: {
     default: defaultSlides
   }
 }
 
-function defaultSlides(num = 20) {
+export const OneSlideInfinite = {
+    args: {
+        default: defaultSlides,
+        infinite: true
+    }
+}
+
+export const TwoSlideGroupsFinite = {
+    args: {
+        default: defaultSlides(),
+        groupsToShow: 2
+    }
+}
+
+function defaultSlides(num = 10) {
   return Array.from({ length: num }, (_, i) => {
     return h(
       'div',
       {
-        key: i
+        key: i,
+        class: 'slide'
       },
       [
-        h('h1', {}, `Slide ${i + 1}`),
-        h('p', {}, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
+        h('h1', { class: 'no-swipe' }, `Slide ${i + 1}`),
+        h('img', { src: `https://picsum.photos/800/400?random=${i}`, class: 'no-swipe' }),
+        h('p', { class: 'no-swipe' }, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
       ]
     )
   })
