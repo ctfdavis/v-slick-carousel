be9b525ab10c46798c9f08661af71e45b7ef294e
λ/dragon <davis.chan@aol.com>
2024-09-05 10:02:26 +0800

    test: storybook test for lazy load on demand

---
+++ packages/storybook/src/tests/lazy-load-on-demand/index.stories.ts
@@ -0,0 +1,25 @@
+import { userEvent, within } from '@storybook/test'
+import { LazyLoadOnDemand } from '../../VSlickCarousel.stories.ts'
+import { argInfo } from '../../common.ts'
+import { VSlickCarousel } from 'v-slick-carousel'
+import { wait } from '../utils.ts'
+
+export default {
+  component: VSlickCarousel,
+  tags: [],
+  ...argInfo
+}
+
+export const Test = {
+  ...LazyLoadOnDemand,
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await userEvent.click(canvas.getByRole('slide'))
+    await wait(1000)
+    await userEvent.keyboard('[ArrowRight]')
+    await wait(1000)
+    await userEvent.keyboard('[ArrowRight]')
+    await wait(1000)
+    await userEvent.keyboard('[ArrowRight]')
+  }
+}
